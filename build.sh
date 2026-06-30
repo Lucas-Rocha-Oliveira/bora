2c9fbbd35c3d4fcdebb3ae1a13f3d30955594b7f
Luiz-Felipe-M-O <luizfelipemeloliveira@gmail.com>
2026-06-30 04:26:02 -0300

    v2

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Sai imediatamente se qualquer comando falhar.
-set -o errexit
-
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
